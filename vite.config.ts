a72b9d061c3b2e6bc209621a1f91e764a1d0b966
Adhitya Giva Muhammad <adhitya.giva@gmail.com>
2026-01-05 21:39:33 +0700

    [Update][Wrangler] - Add config wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare({ viteEnvironment: { name: 'ssr' } })],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src')
